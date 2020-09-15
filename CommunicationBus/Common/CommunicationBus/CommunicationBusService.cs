cfe154a3eda3d223e00d5ac236c602878a24ec80
munja97 <64159013+munja97@users.noreply.github.com>
2020-09-15 19:05:52 +0200

    Testovi poceti.

---
+++ CommunicationBus/Common/CommunicationBus/CommunicationBusService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Common.CommunicationBus
+{
+    public class CommunicationBusService
+    {
+        public XNode Node{ get; set; }
+        XmlToDBAdapter xMLToSQL = new XmlToDBAdapter();
+
+        public CommunicationBusService()
+        {
+        }
+
+        public CommunicationBusService(XNode node)
+        {
+
+            Node = node;
+        }
+        public string Run(XNode node = null)
+        {
+            if(node != null)
+            {
+                Node = node;
+            }
+            return xMLToSQL.Convert(Node);
+        }
+    }
+}
---
+++ CommunicationBus/CommunicationBus/Program.cs
@@ -0,0 +1,40 @@
+﻿using Common.CommunicationBus;
+using Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CommunicationBus;
+using Newtonsoft.Json;
+
+namespace CommunicationBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringToJSONConverter stringToJSON = new StringToJSONConverter();
+            string URL = "";
+
+            while (true)
+            {
+                Console.WriteLine("Unesite URL: ");
+                URL = Console.ReadLine();
+  
+                string jsonRequest = stringToJSON.Convert(URL);
+                Console.WriteLine(jsonRequest);
+
+                XNode xmlNode = JsonConvert.DeserializeXNode(jsonRequest, "Request");
+
+                CommunicationBusService CBService = new CommunicationBusService(xmlNode);
+                Console.WriteLine(CBService.Run(xmlNode));
+
+                XElement element = xmlNode.Document.Element("Request");
+            }
+
+        }
+        
+    }
+}
