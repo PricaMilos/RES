75aa71d721115c2363d9a8b9e1ca154f59c9a993
munja97 <64159013+munja97@users.noreply.github.com>
2020-09-16 22:10:09 +0200

    Zavrseno.

---
+++ CommunicationBus/Common/CommunicationBus/StringToJSONConverter.cs
@@ -0,0 +1,90 @@
+﻿using Common.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.CommunicationBus
+{
+    public class StringToJSONConverter
+    {
+        public StringToJSONConverter()
+        {
+
+        }
+        public string Convert(string stringRequest)
+        {
+            Request Request = new Request();
+            string[] delovi = stringRequest.Split('/');
+            String metod = delovi[0].ToUpper();
+
+            Request.Verb = delovi[0];
+            
+            if (metod == "GET")
+            {
+                //GET/Resurs
+                Request.Noun = delovi[1];
+                //GET/Resurs/1
+                if (delovi.Length > 2 && !String.IsNullOrEmpty(delovi[2]))
+                {
+                    Request.Noun = delovi[1] + "/" + delovi[2];
+                }
+
+                //get/Resurs/1/Naziv='pera'/
+                //get/Resurs/1/Naziv='pera'
+                if (delovi.Length >= 4) 
+                { 
+                    Request.Query = delovi[3]; 
+                }
+                //get/Resurs/1/Naziv='resurs'/Id;Naziv
+                //get/Resurs//Naziv='resurs'/Id;Naziv;Opis
+                //get/Resurs/1//Id;Naziv;surname
+                if (delovi.Length == 5)
+                {
+                    Request.Fields = delovi[4];
+                }
+            }
+            else if (metod == "POST")
+            {
+                Request.Noun = delovi[1];
+                Request.Query = delovi[2];
+                if (delovi.Length >= 4)
+                {
+                    Request.Query = delovi[3];
+                }
+                //get/Resurs/1/Naziv='resurs'/Id;Naziv
+                //get/Resurs//Naziv='resurs'/Id;Naziv;Opis
+                //get/Resurs/1//Id;Naziv;surname
+                if (delovi.Length == 5)
+                {
+                    Request.Fields = delovi[4];
+                }
+            }
+            else if (metod == "PATCH")
+            {
+                Request.Noun = delovi[1] + "/" + delovi[2];
+                if (delovi.Length >= 4)
+                {
+                    Request.Query = delovi[3];
+                }
+
+                if (delovi.Length == 5)
+                {
+                    Request.Fields = delovi[4];
+                }
+            }
+            else if (metod == "DELETE")
+            {
+                Request.Noun = delovi[1] + "/" + delovi[2];
+            }
+            else
+            {
+                return null;
+            }
+
+            return JsonConvert.SerializeObject(Request, Formatting.Indented);
+        }
+    }
+}
---
+++ CommunicationBus/Tests/TestStringToJSON.cs
@@ -0,0 +1,87 @@
+﻿using Common.CommunicationBus;
+using Common.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestStringToJSON
+    {
+        [TestMethod]
+        public void TestGet_Success()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("GET/Resources/1");
+            
+            Request Request = new Request("GET", "/Resources/1", null, null);
+            string expectedJson = JsonConvert.SerializeObject(Request, Formatting.Indented);
+
+            Assert.AreEqual(json, expectedJson);
+        }
+
+        [TestMethod]
+        public void Test_Error()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("fqwef/Resources/1");
+
+            Assert.IsNull(json);
+        }
+
+        [TestMethod]
+        public void TestGet_Success2()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("GET/Resources/1/name='Pera'/name;surname");
+            Request Request = new Request("GET", "/Resources/1", "name='Pera'", "name;surname");
+            string expectedJson = JsonConvert.SerializeObject(Request, Formatting.Indented);
+
+            Assert.AreEqual(json, expectedJson);
+        }
+
+        [TestMethod]
+        public void TestDelete_Success()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("DELETE/Resources/1");
+            Request Request = new Request("DELETE", "/Resources/1", null, null);
+            string expectedJson = JsonConvert.SerializeObject(Request, Formatting.Indented);
+
+            Assert.AreEqual(json, expectedJson);
+        }
+
+        [TestMethod]
+        public void TestPOST_Success()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("POST/Resources/1");
+            Request Request = new Request("POST", "/Resources", "1", null);
+            string expectedJson = JsonConvert.SerializeObject(Request, Formatting.Indented);
+
+            Assert.AreEqual(json, expectedJson);
+        }
+
+        [TestMethod]
+        public void TestPATCH_Success()
+        {
+            StringToJSONConverter converter = new StringToJSONConverter();
+
+            string json = converter.Convert("PATCH/Resources/1/query/fields");
+            Request Request = new Request("PATCH", "/Resources/1", "query", "fields");
+            string expectedJson = JsonConvert.SerializeObject(Request, Formatting.Indented);
+
+            Assert.AreEqual(json, expectedJson);
+        }
+    }
+}
